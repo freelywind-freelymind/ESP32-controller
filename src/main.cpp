768b460adcf9fb33111235a3d916f52e7bdd125f
freelywind <ken.12330@yahoo.com.hk>
2022-05-17 01:45:41 +0800

    add more butts

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define butt1_pin 35
-#define butt2_pin 32
-#define butt3_pin 21
-#define butt4_pin 19
+#define butt1_pin 32
+#define butt2_pin 33
+#define butt3_pin 25
+#define butt4_pin 26
+#define butt5_pin 23
+#define butt6_pin 22
+#define butt7_pin 21
+#define butt8_pin 19
 
 // REPLACE WITH YOUR RECEIVER MAC Address
 uint8_t upperAddress[] = {0x58, 0xBF, 0x25, 0x18, 0xB4, 0x84};
@@ -87,6 +91,66 @@ void butt4_interrupt(){
   }
 }
 
+void butt5_interrupt(){
+  myData.butt_no = '5';
+  Serial.printf("Button %d had been pressed.", myData.butt_no);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(upperAddress, (uint8_t *) &myData, sizeof(myData));
+    
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
+void butt6_interrupt(){
+  myData.butt_no = '6';
+  Serial.printf("Button %d had been pressed.", myData.butt_no);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(upperAddress, (uint8_t *) &myData, sizeof(myData));
+    
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
+void butt7_interrupt(){
+  myData.butt_no = '7';
+  Serial.printf("Button %d had been pressed.", myData.butt_no);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(upperAddress, (uint8_t *) &myData, sizeof(myData));
+    
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
+void butt8_interrupt(){
+  myData.butt_no = '8';
+  Serial.printf("Button %d had been pressed.", myData.butt_no);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(upperAddress, (uint8_t *) &myData, sizeof(myData));
+    
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -122,15 +186,23 @@ void setup() {
     return;
   }
 
-  pinMode(butt1_pin, INPUT_PULLDOWN);
-  pinMode(butt2_pin, INPUT_PULLDOWN);
-  pinMode(butt3_pin, INPUT_PULLDOWN);
-  pinMode(butt4_pin, INPUT_PULLDOWN);
+  pinMode(butt1_pin, INPUT_PULLUP);
+  pinMode(butt2_pin, INPUT_PULLUP);
+  pinMode(butt3_pin, INPUT_PULLUP);
+  pinMode(butt4_pin, INPUT_PULLUP);
+  pinMode(butt5_pin, INPUT_PULLUP);
+  pinMode(butt6_pin, INPUT_PULLUP);
+  pinMode(butt7_pin, INPUT_PULLUP);
+  pinMode(butt8_pin, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(butt1_pin), butt1_interrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(butt2_pin), butt2_interrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(butt3_pin), butt3_interrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(butt4_pin), butt4_interrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(butt1_pin), butt1_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt2_pin), butt2_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt3_pin), butt3_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt4_pin), butt4_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt5_pin), butt1_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt6_pin), butt2_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt7_pin), butt3_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butt8_pin), butt4_interrupt, FALLING);
 }
 
 void loop() {
