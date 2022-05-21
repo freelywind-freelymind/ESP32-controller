8f26ef0b79b185bfcae1c7c755425b6cf022543c
freelywind <ken.12330@yahoo.com.hk>
2022-05-22 02:42:37 +0800

    add pinmap and some function

---
+++ src/joystick_update.h
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include "pinmap.h"
+
+int axis[4];
+float softening = 0.2;
+float max_speed = 1;
+float max_rotation_speed = 1;
+int deadband = 100; 
+
+float mapfloat(long x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
+
+float soften_value(float axis, float soften, float max_v){
+  return axis * pow(abs(axis/max_v), soften);
+}
+
+void joystick_update(){
+  int temp = 0;
+
+  temp = analogRead(Ljoystick_x_pin);
+  if(temp < 0) {
+    axis[0] = -(temp) - 2046;
+  }
+  else{
+    axis[0] = 2047 - temp;
+  }
+
+  temp = analogRead(Ljoystick_y_pin);
+  if(temp < 0) {
+    axis[1] = -(temp) - 2046;
+  }
+  else{
+    axis[1] = 2047 - temp;
+  }
+
+  //temp = analogRead(Rjoystick_x_pin);
+  //if(temp < 0) {
+  //  axis[2] = -(temp) - 2046;
+  //}
+  //else{
+  //  axis[2] = 2047 - temp;
+  //}
+
+  //temp = analogRead(Rjoystick_y_pin);
+  //if(temp < 0) {
+  //  axis[3] = -(temp) - 2046;
+  //}
+  //else{
+  //  axis[3] = 2047 - temp;
+  //}
+}
---
+++ src/pinmap.h
@@ -0,0 +1,22 @@
+#define Ljoystick_x_pin 34
+#define Ljoystick_y_pin 35
+#define L1_pin  39
+#define L2_pin  36
+
+#define up_pin 32
+#define left_pin 33
+#define down_pin 25 
+#define right_pin 26
+
+#define Rjoystick_x_pin 4   //have problem
+#define Rjoystick_y_pin 2   //have problem
+#define R1_pin  23
+#define R2_pin  22
+
+#define triangle_pin 19
+#define circle_pin 21
+#define square_pin 17
+#define cross_pin 18
+
+#define question_pin 27
+#define estop_pin 16
