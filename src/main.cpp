6a59fe35b2a4d3dd21305e0401878e56edbb2362
freelywind <ken.12330@yahoo.com.hk>
2022-05-23 23:16:54 +0800

    send msg through can bus

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <CAN.h>
+
+const int rx = 18;
+const int tx = 19;
+const long baudrate = 1000000;
+const int package_id = 0x12;
+const int package_size = 1;
 
 typedef struct upper_msg {
   char butt_no;
@@ -19,7 +26,7 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
  
 void setup() {
   // Initialize Serial Monitor
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -35,6 +42,13 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
+
+  CAN.setPins(rx, tx);
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(baudrate)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
 }
  
 void loop() {
@@ -43,7 +57,10 @@ void loop() {
     Serial.print(upper_order.butt_no);
     Serial.println();*/
 
-    Serial.print(upper_order.butt_no);
+    CAN.beginPacket(package_id, 1);
+    CAN.write(upper_order.butt_no);
+    //send the package
+    CAN.endPacket();
 
     flag = false;
   }
