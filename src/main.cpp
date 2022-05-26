21f99725284a2b0c770f39e2f9b54a1da1856e2e
freelywind <ken.12330@yahoo.com.hk>
2022-05-26 21:48:52 +0800

    update package struct

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,20 @@ const long baudrate = 1000000;
 const int package_id = 0x15;
 const int package_size = 6;
 
-typedef struct chassis_msg {
+typedef struct package {
   int16_t x_axis;
   int16_t y_axis;
   int16_t w_axis;
-} chassis_msg;
+  char butt_no;
+} package;
 
-chassis_msg chassis_order;
+package msg;
 
 volatile bool flag = false;
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&chassis_order, incomingData, sizeof(chassis_order));
+  memcpy(&msg, incomingData, sizeof(msg));
   flag = true;
 }
  
@@ -38,7 +39,7 @@ void setup() {
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    //Serial.println("Error initializing ESP-NOW");
+    Serial.println("Error initializing ESP-NOW");
     return;
   }
   
@@ -57,19 +58,19 @@ void setup() {
 void loop() {
   if(flag){
     Serial.print("X: ");
-    Serial.print(chassis_order.x_axis);
+    Serial.print(msg.x_axis);
     Serial.print(" Y: ");
-    Serial.print(chassis_order.y_axis);
+    Serial.print(msg.y_axis);
     Serial.print(" W: ");
-    Serial.println(chassis_order.w_axis);
+    Serial.println(msg.w_axis);
 
     CAN.beginPacket(package_id, package_size);
-    CAN.write(chassis_order.x_axis >> 8);
-    CAN.write(chassis_order.x_axis & 0xFF);
-    CAN.write(chassis_order.y_axis >> 8);
-    CAN.write(chassis_order.y_axis & 0xFF);
-    CAN.write(chassis_order.w_axis >> 8);
-    CAN.write(chassis_order.w_axis & 0xFF);
+    CAN.write(msg.x_axis >> 8);
+    CAN.write(msg.x_axis & 0xFF);
+    CAN.write(msg.y_axis >> 8);
+    CAN.write(msg.y_axis & 0xFF);
+    CAN.write(msg.w_axis >> 8);
+    CAN.write(msg.w_axis & 0xFF);
     //send the package
     CAN.endPacket();    
 
